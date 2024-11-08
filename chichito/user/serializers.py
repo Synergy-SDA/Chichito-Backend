9611fb34897bf91a99b9af810aea3d02770946ef
hosein <hosein.shah.hoseni.mirash@gmail.com>
2024-11-08 19:05:29 +0330

    login

--- chichito/user/serializers.py
+++ chichito/user/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from .models import User
+from django.contrib.auth import authenticate
+from rest_framework.exceptions import AuthenticationFailed
+
 
 class UserSignupSerializer(serializers.ModelSerializer):
     password1 = serializers.CharField(write_only=True, required=True, min_length=8)
@@ -30,3 +33,19 @@ class UserSignupSerializer(serializers.ModelSerializer):
         user.save()
         
         return user
+
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
+
+    def validate(self, data):
+        user = authenticate(username=data['username'], password=data['password'])
+
+        if not user:
+            raise AuthenticationFailed('Invalid username or password')
+
+        data['user'] = user
+        return data
+
--- chichito/user/views.py
+++ chichito/user/views.py
@@ -2,12 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import User
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import UserSignupSerializer
+from .serializers import UserSignupSerializer , UserLoginSerializer
+from django.contrib.auth import authenticate
 
 class UserSignupView(APIView):
     def post(self, request):
@@ -23,3 +20,14 @@ class UserSignupView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class UserLoginView(APIView):
+    def post(self , requset):
+        serializer = UserLoginSerializer(data=requset.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "refresh": str(refresh),
+                "access": str(refresh.access_token)
+            }, status=status.HTTP_200_OK)
+        return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
