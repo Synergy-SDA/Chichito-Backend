8a0e72460bc030e244983591268d0163cf433a14
hosein <hosein.shah.hoseni.mirash@gmail.com>
2024-11-08 18:53:28 +0330

    signup done

--- chichito/chichito/settings.py
+++ chichito/chichito/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
--- chichito/user/serializers.py
+++ chichito/user/serializers.py
@@ -2,25 +2,31 @@ from rest_framework import serializers
 from .models import User
 
 class UserSignupSerializer(serializers.ModelSerializer):
-    password1 = serializers.CharField(write_only=True, required=True)
-    password2 = serializers.CharField(write_only=True, required=True)
+    password1 = serializers.CharField(write_only=True, required=True, min_length=8)
+    password2 = serializers.CharField(write_only=True, required=True, min_length=8)
 
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'phone_number', 'mail', 'birth_date', 'address', 'sex', 'password1', 'password2']
         extra_kwargs = {
-            'mail': {'required': True}
+            'username': {'required': True, 'min_length': 5},
+            'mail': {'required': True},
+            'first_name': {'required': True},
+            'last_name': {'required': True},
         }
 
     def validate(self, data):
         if data['password1'] != data['password2']:
-            raise serializers.ValidationError("Passwords do not match.")
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+        
         return data
 
     def create(self, validated_data):
         password = validated_data.pop('password1')
         validated_data.pop('password2')
+        
         user = User.objects.create_user(**validated_data)
         user.set_password(password)
         user.save()
+        
         return user
