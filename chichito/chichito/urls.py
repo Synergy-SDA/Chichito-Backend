424179c5fdc6eda50f239b437b1cb52a167c9416
Ali-hamzehi <alihamzehiesfahani@gmail.com>
2024-11-18 17:01:08 +0330

    add product urls

--- chichito/chichito/urls.py
+++ chichito/chichito/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('user.urls')), 
     # path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/', include('product.urls')), 
 ]
