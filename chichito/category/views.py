8d3e1909139f5cd4bf82027e9af3a3191b79d477
hosein <hosein.shah.hoseni.mirash@gmail.com>
2024-11-21 18:10:34 +0330

    crud category

---
+++ chichito/category/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from .serializers import CategorySerializer 
+from .models import Category
+
+class CategoryAPIView(APIView):
+    
+
+    def post(self, request, *args, **kwargs):
+        
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def get(self, request, pk=None, *args, **kwargs):
+        if pk:
+            try:
+                category = Category.objects.get(pk=pk)
+            except Category.DoesNotExist:
+                return Response({"detail": "Category not found."}, status=status.HTTP_404_NOT_FOUND)
+            serializer = CategorySerializer(category)
+            return Response(serializer.data)
+        else:
+            categories = Category.objects.all()
+            serializer = CategorySerializer(categories, many=True)
+            return Response(serializer.data)
+        
+    def put(self, request, pk, *args, **kwargs):
+        try:
+            category = Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            return Response({"detail": "Category not found."}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, pk, *args, **kwargs):
+        try:
+            category = Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            return Response({"detail": "Category not found."}, status=status.HTTP_404_NOT_FOUND)
+        
+        category.delete()
+        return Response({"detail": "Category deleted."}, status=status.HTTP_204_NO_CONTENT)
--- chichito/chichito/settings.py
+++ chichito/chichito/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user',
+    'category',
     'rest_framework',
     'drf_yasg',
     'corsheaders',
