225060f1b885c6b9b3e15b36466318ca6e0fbd04
hosein <hosein.shah.hoseni.mirash@gmail.com>
2024-12-12 16:44:29 +0330

    signal

--- cart/apps.py
+++ cart/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
+# apps.py
+from django.apps import AppConfig
 
-class CartConfig(AppConfig):
+class YourAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cart'
+
+    def ready(self):
+        import cart.signal # Ensure signals are loaded
