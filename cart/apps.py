e1139fe0861220066e3b774c30e684bc68730a44
Ali-hamzehi <alihamzehiesfahani@gmail.com>
2024-12-12 20:01:38 +0330

    feat : add signal and register model

--- cart/apps.py
+++ cart/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
-
+from signals import *
 
 class CartConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cart'
+    def ready(self):
+        import cart.signals
