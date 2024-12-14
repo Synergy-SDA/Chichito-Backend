9f51f5101f59b4b2fec3774108901582a000a3a8
Ali-hamzehi <alihamzehiesfahani@gmail.com>
2024-12-14 20:23:46 +0330

    fix : fix admin of product and category

--- category/admin.py
+++ category/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Category
 # Register your models here.
-admin.site.register(Category)
+from django.contrib import admin
+from .models import Category
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'parent', 'description')  
+    list_filter = ('parent',)  
+    search_fields = ('name', 'description')  
+    ordering = ('name',)  
+    list_editable = ('description',)  
+
+
+admin.site.register(Category, CategoryAdmin)
+
